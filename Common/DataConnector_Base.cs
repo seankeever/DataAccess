cabd6be0196e8b046272d42e19e75ff0e36f652a
Sean Keever <sean@ravenlogistics.com>
2020-07-17 12:39:42 -0400

    inital commit to push all exisitng code to new data access repo

---
+++ Common/DataConnector_Base.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccess.Common
+{
+	public class ConnectionManagement
+	{
+		private SqlConnection _connection;
+		private string _connectionstring;
+
+		public ConnectionManagement(string sConnectionString)
+		{
+			_connectionstring = sConnectionString;
+
+		}
+
+		public SQLQueryResult ExecuteNonQueryProcedure(string storedprocedureName, List<SqlParameter> paramList, int timeout_overwrite = 0)
+		{
+			SQLQueryResult res = new SQLQueryResult();
+			_connection = new SqlConnection(_connectionstring);
+
+			try
+			{
+				SqlCommand objCmd = new SqlCommand();
+				objCmd.Connection = _connection;
+
+				objCmd.CommandType = CommandType.StoredProcedure;
+				objCmd.CommandText = storedprocedureName;
+
+				if (timeout_overwrite > 0)
+					objCmd.CommandTimeout = timeout_overwrite;
+
+				SqlDataAdapter objAdapter = new SqlDataAdapter();
+				objAdapter.SelectCommand = objCmd;
+
+				//Parameter List
+				foreach (SqlParameter param in paramList)
+				{
+					try
+					{
+						if (param.Value != null && param.Value.ToString() != string.Empty)
+							objCmd.Parameters.Add(param);
+						else
+							objCmd.Parameters.Add(new SqlParameter(param.ParameterName, DBNull.Value));
+					}
+					catch (Exception ex)
+					{
+					}
+				}
+
+				_connection.Open();
+				objCmd.ExecuteNonQuery();
+
+				//Checking for output variables
+				foreach (SqlParameter param in paramList)
+				{
+					try
+					{
+						if (param.Direction == ParameterDirection.Output)
+						{
+							string paramKey = param.ParameterName.ToString();
+							string paramValue = param.Value.ToString();
+							res.OutputParams.Add(paramKey, paramValue);
+						}
+					}
+					catch (Exception ex)
+					{
+					}
+				}
+			}
+			catch (SqlException ex)
+			{
+				res.SqlError = ex;
+			}
+
+			try
+			{
+				_connection.Close();
+			}
+			catch (Exception ex)
+			{
+			}
+
+			return res;
+		}
+
+		public SQLQueryResult ExecuteQueryProcedure(string storedprocedureName, List<SqlParameter> paramList, int timeout_overwrite = 0)
+		{
+			SQLQueryResult res = new SQLQueryResult();
+			_connection = new SqlConnection(_connectionstring);
+
+			try
+			{
+				SqlCommand objCmd = new SqlCommand();
+				objCmd.Connection = _connection;
+
+				objCmd.CommandType = CommandType.StoredProcedure;
+				objCmd.CommandText = storedprocedureName;
+
+				if (timeout_overwrite > 0)
+					objCmd.CommandTimeout = timeout_overwrite;
+
+				SqlDataAdapter objAdapter = new SqlDataAdapter();
+				objAdapter.SelectCommand = objCmd;
+
+				//Parameter List
+				foreach (SqlParameter param in paramList)
+				{
+					try
+					{
+						if (param.Value != null && param.Value.ToString() != string.Empty)
+							objCmd.Parameters.Add(param);
+						else
+							objCmd.Parameters.Add(new SqlParameter(param.ParameterName, DBNull.Value));
+					}
+					catch (Exception ex)
+					{
+					}
+				}
+
+				objAdapter.Fill(res.QueryOutput, "OutputDataSet");
+
+				//Checking for output variables
+				foreach (SqlParameter param in paramList)
+				{
+					try
+					{
+						if (param.Direction == ParameterDirection.Output)
+						{
+							string paramKey = param.ParameterName.ToString();
+							string paramValue = param.Value.ToString();
+							res.OutputParams.Add(paramKey, paramValue);
+						}
+					}
+					catch (Exception ex)
+					{
+					}
+				}
+			}
+			catch (SqlException ex)
+			{
+				res.SqlError = ex;
+			}
+
+			return res;
+		}
+
+	}
+
+	public class SQLQueryResult
+	{
+		public SqlException SqlError;
+		public DataSet QueryOutput;
+		public Dictionary<string, string> OutputParams;
+		public SqlDataReader DataReader;
+
+		public SQLQueryResult()
+		{
+			SqlError = null;
+			QueryOutput = new DataSet();
+			OutputParams = new Dictionary<string, string>();
+			DataReader = null;
+		}
+	}
+}
---
+++ SurveyAccess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class SurveyAccess
+    {
+        public string _connectionstring;
+        public string _connectionstringName = "SurveyManagement";
+        public SqlException _lastSqlException;
+
+        public SurveyAccess()
+        {
+            _connectionstring = System.Configuration.ConfigurationManager.ConnectionStrings[_connectionstringName].ToString();
+        }
+
+		public bool InsertQuestion(string Description, bool AllowYesNo, bool AllowShortAnswer)
+		{
+			Common.ConnectionManagement myconnector = new Common.ConnectionManagement(_connectionstring);
+			List<SqlParameter> procParams = new List<SqlParameter>();
+			procParams.Add(new SqlParameter("@Description", Description));
+			procParams.Add(new SqlParameter("@AllowYesNo", AllowYesNo));
+			procParams.Add(new SqlParameter("@AllowShortAnswer", AllowShortAnswer));
+			Common.SQLQueryResult queryRes = myconnector.ExecuteNonQueryProcedure("srvy.InsertQuestion", procParams, 600);
+			_lastSqlException = queryRes.SqlError;
+			if (queryRes.SqlError != null)
+				return false;
+			else
+				return true;
+
+		}
+	}
+}
